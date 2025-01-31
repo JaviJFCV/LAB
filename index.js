291d516a175769c30c3336dc65377071a3869b2d
JaviJFCV <catalan.javierafernanda@gmail.com>
2025-01-31 19:03:11 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Endpoint para obtener los datos de la API externa
+app.get('/insurance', async (req, res) => {
+    try {
+        const response = await axios.get('https://dn8mlk7hdujby.cloudfront.net/interview/insurance/58');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener los datos' });
+    }
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
